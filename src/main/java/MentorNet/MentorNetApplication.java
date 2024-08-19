9d6f0021d77b8f90d006ed5300ae04d4165deabf
Yurii Bolharyn <yurecpr@gmail.com>
2024-08-19 11:57:00 +0200

    edit

--- src/main/java/MentorNet/MentorNetApplication.java
+++ src/main/java/MentorNet/MentorNetApplication.java
@@ -1,4 +1,4 @@
-package com.example.MentorNet;
+package MentorNet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/MentorNet/MentorNetApplicationTests.java
+++ src/test/java/MentorNet/MentorNetApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.MentorNet;
+package MentorNet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
