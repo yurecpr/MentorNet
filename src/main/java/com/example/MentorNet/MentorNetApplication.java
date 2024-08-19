422ebb6e0d26e38970b0f789cf063f3801f79a2b
Yurii Bolharyn <yurecpr@gmail.com>
2024-08-19 11:49:25 +0200

    first commit

---
+++ src/main/java/com/example/MentorNet/MentorNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.MentorNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorNetApplication.class, args);
+	}
+
+}
